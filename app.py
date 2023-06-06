e307fe00ae0e112f80ca1acadd27967c6c92c07b
leecheng504 <deak910504@gmail.com>
2023-06-06 18:39:35 +0800

    123

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "Hello Flask"
+
+
+@app.route("/test")
+def test():
+    return "this is test"
+
+
+if __name__ == "__main__":
+    app.run()
