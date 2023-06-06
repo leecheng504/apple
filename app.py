aef8cb9710404342dc297b7f246df639cd272b0e
leecheng504 <deak910504@gmail.com>
2023-06-06 19:41:00 +0800

    4

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) # __name__代表目前執行的模組
 
 
-@app.route("/")
+@app.route("/") #已函示為基礎，提供附加的功能
 def home():
     return "Hello 這是我們專題的網頁"
 
@@ -12,5 +12,5 @@ def test():
     return "hi"
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == "__main__": #如果以主程式執行 
+    app.run() #立即啟動伺服器
