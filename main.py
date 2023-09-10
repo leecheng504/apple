e7bd7debbfb3d384f845d3180d73c955aa97dcc0
leecheng504 <deak910504@gmail.com>
2023-09-10 15:16:36 +0800

    debuggg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)  # __name__代表目前執行的模組
+Bootstrap(app)
 
 
 @app.route("/")  # 已函示為基礎，提供附加的功能
@@ -7,10 +8,5 @@ def home():
     return render_template("introduce.html")
 
 
-@app.route("/test")
-def test():
-    return "hi"
-
-
 if __name__ == "__main__":  # 如果以主程式執行
-    app.run()  # 立即啟動伺服器
+    app.run(debug=True)  # 立即啟動伺服器
